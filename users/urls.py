c7b1c9b3891db3705a75d83793cebe8a6dd2d916
Javvadhi <javvadhiraghu@gmail.com>
2021-06-06 15:51:45 +0530

    Update urls.py

--- users/urls.py
+++ users/urls.py
@@ -5,7 +5,7 @@ from users import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('post', views.post),  
-    path('get/<int:id>', views.get),
-    path('update/<int:id>', views.update),
-    path('delete/<int:id>', views.destroy),       
+    path('get/<int:uid>', views.get),
+    path('update/<int:uid>', views.update),
+    path('delete/<int:uid>', views.destroy),       
 ]
