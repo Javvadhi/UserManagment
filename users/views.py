a80c70c10e7fe921dd4803cffbdcab031852e893
Javvadhi <javvadhiraghu@gmail.com>
2021-06-07 06:56:47 +0530

    Add files via upload

--- users/views.py
+++ users/views.py
@@ -1,35 +1,23 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render
 from users.models import User
-from users.forms import UserForm
+from django.http import JsonResponse
 
 def post(request):
-    if request.method == "POST":
-        form = UserForm(request.POST)
-        if form.is_valid():
-            try:
-                form.save()
-            except:
-                pass 
-    else:
-        form = UserForm
-
-    return render(request,'temp.html',{'form':form})
+    users = User.objects.save()
+    return JsonResponse({'users':users})
 
 def get(request,uid):
     users = User.objects.get(uid=uid)
-    return render(request.'temp.html',{'users':users})
+    return JsonResponse({'users':users})
 
 def update(request,uid):
     users = User.objects.get(uid=uid)
-    form =UserForm(request.POST, instance = users) 
-    if form.is_valid():
-        form.save()
-    return render(request.'temp.html',{'form':form})
+    users = users.objects.save()
+    return JsonResponse({'users':users})
 
 def delete(request,uid):
     users = User.objects.get(uid=uid)
     users.delete()
-    return redirect("html")
 
 
 
